d2885317400e72dabaffe98f7c7583ccbef8851c
Michele Berry <michelelberry@gmail.com>
2023-08-14 23:19:57 -0400

    add json return

--- tutorial_1/app.py
+++ tutorial_1/app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
     return "Hello World!"
+
+@app.route('/hithere')
+def hi():
+    return 'Yo'
+
+@app.route('/goodbye')
+def bye():
+    me = {
+        'Name': 'Apple',
+        'German': 'Apfel',
+        'Colors': [
+            {
+                'name': 'red',
+                'price': 1.5
+            },
+            {
+                'name': 'green',
+                'price': 2
+            }
+        ]
+    }
+    return jsonify(me)
     
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', debug=True)
