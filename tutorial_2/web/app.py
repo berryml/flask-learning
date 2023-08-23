f60bb7c209a298f00007d3ba49600700eca0d9e7
Michele Berry <michelelberry@gmail.com>
2023-08-22 22:53:32 -0400

    got mongo to authenticate

--- tutorial_2/web/app.py
+++ tutorial_2/web/app.py
@@ -101,6 +101,5 @@ api.add_resource(Divide, '/divide')
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=True)
-    
-    
+
 # http://127.0.0.1:5001
